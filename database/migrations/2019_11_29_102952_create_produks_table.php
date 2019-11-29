5be1ff6620d7ad4158c398bfb8b62977095cc508
Winnertj <55679218+Winnertj@users.noreply.github.com>
2019-11-29 18:45:49 +0700

    Produk
    buat produkcontroller dan model produk

---
+++ database/migrations/2019_11_29_102952_create_produks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProduksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('merek',50);
+            $table->string('ukuran',50);
+            $table->string('warna',50);
+            $table->text('description');
+            $table->integer('harga');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produks');
+    }
+}
